58df546573fa91884044830f1b9aca8a06b8ef14
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-06 16:11:12 -0400

    working Monday forecast

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import DailyInfo from "./DailyInfo";
-// import DailyInfo from "./DailyInfo"
+import Image from "./Image"
 
 class HomePage extends React.Component{
     constructor(props){
@@ -28,19 +28,19 @@ class HomePage extends React.Component{
         try{
            const location= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=40.7143&lon=-74.006&units=imperial&appid=${key}`);
            this.setState({mondayTemp: location.data.daily[0].temp});
-           this.setState({tuesdayTemp: location.data.daily[1].temp});
-           this.setState({wednesdayTemp: location.data.daily[2].temp});
-           this.setState({thursdayTemp: location.data.daily[3].temp});
-           this.setState({fridayTemp: location.data.daily[4].temp});
-           this.setState({saturdayTemp: location.data.daily[5].temp});
-           this.setState({sundayTemp: location.data.daily[6].temp});   
+        //    this.setState({tuesdayTemp: location.data.daily[1].temp});
+        //    this.setState({wednesdayTemp: location.data.daily[2].temp});
+        //    this.setState({thursdayTemp: location.data.daily[3].temp});
+        //    this.setState({fridayTemp: location.data.daily[4].temp});
+        //    this.setState({saturdayTemp: location.data.daily[5].temp});
+        //    this.setState({sundayTemp: location.data.daily[6].temp});   
            this.setState({mondayImg: location.data.daily[0].weather});
-           this.setState({tuesdayImg: location.data.daily[1].weather});
-           this.setState({wednesdayImg: location.data.daily[2].weather});
-           this.setState({thursdayImg: location.data.daily[3].weather});
-           this.setState({fridayImg: location.data.daily[4].weather});
-           this.setState({saturdayImg: location.data.daily[5].weather});
-           this.setState({sundayImg: location.data.daily[6].weather);   
+        //    this.setState({tuesdayImg: location.data.daily[1].weather});
+        //    this.setState({wednesdayImg: location.data.daily[2].weather});
+        //    this.setState({thursdayImg: location.data.daily[3].weather});
+        //    this.setState({fridayImg: location.data.daily[4].weather});
+        //    this.setState({saturdayImg: location.data.daily[5].weather});
+        //    this.setState({sundayImg: location.data.daily[6].weather});   
         }
           catch(e){
             console.log(e);
@@ -53,7 +53,7 @@ class HomePage extends React.Component{
         return (<div>
         <h1>Forecast</h1>
             <h3>Monday:</h3>
-            
+            <Image info={this.state.mondayImg} />
             <DailyInfo info={this.state.mondayTemp} />
 
         </div>)
---
+++ src/components/Image.js
@@ -0,0 +1,20 @@
+import nullCheck from "./nullCheck";
+import React from 'react';
+
+function Image (props){
+    // function weather(){
+        
+    //     let image=nullCheck(props).main;
+    //     console.log(image);
+    //     return image;
+    // }
+    return (
+        <div>
+            {/* {weather()} */}
+            <h1>{nullCheck(props).description}</h1>
+            {/* <img src={require(`./assets/${nullCheck(props).main}.png`)} /> */}
+        </div>
+    );
+};
+
+export default Image;
