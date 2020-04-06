7c1f8bf98314f457f46054b742e7459f9eb737e3
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-06 14:13:08 -0400

    adding days info

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,20 +6,33 @@ class HomePage extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            location:null,
-            // monday:[],
-            // tuesday:[],
-            // wednesday:[],
-            // thursday:[],
-            // friday:[]
+            monday:null,
+            tuesday:null,
+            wednesday:null,
+            thursday:null,
+            friday:null,
+            saturday:null,
+            sunday: null
         }
     }
     async getLocationInfo(){
         const key=process.env.REACT_APP_API_KEY_WEATHER;
         try{
-           const location= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=40.7143&lon=-74.006&appid=${key}`);
-            this.setState({location: location.datay});
-           console.log(this.state.location);  
+           const location= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=40.7143&lon=-74.006&units=imperial&appid=${key}`);
+           this.setState({monday: location.data.daily[0]});
+           this.setState({tuesday: location.data.daily[1]});
+           this.setState({wednesday: location.data.daily[2]});
+           this.setState({thursday: location.data.daily[3]});
+           this.setState({friday: location.data.daily[4]});
+           this.setState({saturday: location.data.daily[5]});
+           this.setState({sunday: location.data.daily[6]});
+           console.log(this.state.monday);  
+           console.log(this.state.tuesday);  
+           console.log(this.state.wednesday);  
+           console.log(this.state.thursday);  
+           console.log(this.state.friday);  
+           console.log(this.state.saturday);  
+           console.log(this.state.sunday);    
         }
           catch(e){
             console.log(e);
