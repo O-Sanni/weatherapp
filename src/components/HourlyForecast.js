fc005472e927e66c80b58b31446341f980613936
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-06 20:18:14 -0400

    update HourlyForecast

--- src/components/HourlyForecast.js
+++ src/components/HourlyForecast.js
@@ -14,28 +14,8 @@ class HourlyForecast extends React.Component{
         const key=process.env.REACT_APP_API_KEY_WEATHER;
         try{
            const location= await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=40.7143&lon=-74.006&units=imperial&appid=${key}`);
-        for (let i=0; i<48; i++){
-            this.setState(data => ({
-                hourlyData: [...data.hourlyData, location.data.hourly[i]]
-              }))
-        }
-        console.log(this.setState.hourlyData[0]);
-        
-        
-           //    this.setState({mondayTemp: location.data.daily[0].temp});
-        //    this.setState({tuesdayTemp: location.data.daily[1].temp});
-        //    this.setState({wednesdayTemp: location.data.daily[2].temp});
-        //    this.setState({thursdayTemp: location.data.daily[3].temp});
-        //    this.setState({fridayTemp: location.data.daily[4].temp});
-        //    this.setState({saturdayTemp: location.data.daily[5].temp});
-        //    this.setState({sundayTemp: location.data.daily[6].temp});   
-        //    this.setState({mondayImg: location.data.daily[0].weather[0]});
-        //    this.setState({tuesdayImg: location.data.daily[1].weather[0]});
-        //    this.setState({wednesdayImg: location.data.daily[2].weather[0]});
-        //    this.setState({thursdayImg: location.data.daily[3].weather[0]});
-        //    this.setState({fridayImg: location.data.daily[4].weather[0]});
-        //    this.setState({saturdayImg: location.data.daily[5].weather[0]});
-        //    this.setState({sundayImg: location.data.daily[6].weather[0]});  
+            this.setState({hourlyData:  location.data.hourly})
+        console.log(this.state.hourlyData);  
         }
           catch(e){
             console.log(e);
@@ -46,7 +26,7 @@ class HourlyForecast extends React.Component{
     }
     render(){
         return(<div>
-
+    <Hourly
         </div>)
     }
 }
