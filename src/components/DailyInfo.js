bbfb59f08c0229c591ceed8831ea831bf6871236
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-06 22:15:59 -0400

    update DailyInfo.js

--- src/components/DailyInfo.js
+++ src/components/DailyInfo.js
@@ -4,8 +4,8 @@ import React from 'react';
 function DailyInfo (props){
     return (
         <div>
-            <p>Day: {Math.trunc(nullCheck(props).day)}F</p>
-            <p>Night: {Math.trunc(nullCheck(props).night)}F</p>
+            <p className="temperature">Day: {Math.trunc(nullCheck(props).day)}F</p>
+            <p className="temperature">Night: {Math.trunc(nullCheck(props).night)}F</p>
         </div>
     );
 };
