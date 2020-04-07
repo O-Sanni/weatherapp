1f43b738fb0bbee960eabdde501fdd68aba9f88e
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-06 22:23:12 -0400

    update Image.js

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react';
 import "./components.css"
 
 function Image (props){
-//got idea how use webpage icons from https://www.youtube.com/watch?v=InoAIgBZIEA
+    //got idea how use webpage icons from https://www.youtube.com/watch?v=InoAIgBZIEA
     return <img id="weatherImg" src={`https://openweathermap.org/img/w/${nullCheck(props).icon}.png`} />
   
 };
