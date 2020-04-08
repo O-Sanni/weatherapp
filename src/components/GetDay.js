8da92dce38a42de47e3d746144c761aa8744b8be
Olga_Sanni <dev.sannikov@gmail.com>
2020-04-08 10:47:04 -0400

    made minor changes

--- src/components/GetDay.js
+++ src/components/GetDay.js
@@ -4,7 +4,7 @@ import React from 'react';
 function GetDay (props){
     {/* Idea for day displaying was get from : https://makitweb.com/convert-unix-timestamp-to-date-time-with-javascript/*/}
     function getData(props){
-    const dayOfWeek=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+    const dayOfWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
        let day= new Date (nullCheck(props)*1000);
        return dayOfWeek[day.getDay()];
     }
